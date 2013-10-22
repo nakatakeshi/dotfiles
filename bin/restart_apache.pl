3a105dc71206d3852aabbfe69dcfb7d8be348f53
nakatakeshi <nakatatakeshi@gmail.com>
2013-10-22 18:15:12 +0900

    modify awk command

--- bin/restart_apache.pl
+++ bin/restart_apache.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-my $cmd = q/ps -ef |grep httpd |grep -v grep |awk '{print $8, $9, $10, $11, "restart"}'/;
+my $cmd = q!ps -ef |awk '$8 ~ /httpd/ {print $8, $9, $10, $11, "restart"}'!;
 my @restart_cmd = `$cmd`;
 for (@restart_cmd) {
     `$_`;
